74cc54f69d5548dc12ec8e097c0f8e1e8120195a
[Wittawat] <[Wittawat335@gmail.com]>
2023-04-06 15:23:13 +0700

    cc

---
+++ BlazorECommerce/Client/Services/Implementations/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using BlazorECommerce.Client.Services.Interfaces;
+using BlazorECommerce.Shared.DTO;
+using BlazorECommerce.Shared.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace BlazorECommerce.Client.Services.Implementations
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ServiceDTO?> Add(Product product)
+        {
+            var result = await _httpClient.PostAsJsonAsync("api/Product", product);
+            return await result.Content.ReadFromJsonAsync<ServiceDTO?>();
+        }
+
+        public async Task<ServiceDTO?> GetById(int id)
+        {
+            var result = await _httpClient.GetAsync($"api/Product/{id}");
+            return await result.Content.ReadFromJsonAsync<ServiceDTO>();
+        }
+
+        public async Task<ServiceDTO?> GetList()
+        {
+            var result = await _httpClient.GetAsync("api/Product");
+            return await result.Content.ReadFromJsonAsync<ServiceDTO>();
+        }
+    }
+}
---
+++ BlazorECommerce/Client/Services/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using BlazorECommerce.Shared.DTO;
+using BlazorECommerce.Shared.Models;
+
+namespace BlazorECommerce.Client.Services.Interfaces
+{
+    public interface IProductService
+    {
+        public Task<ServiceDTO?> Add(Product product);
+        public Task<ServiceDTO?> GetList();
+        public Task<ServiceDTO?> GetById(int id);
+    }
+}
